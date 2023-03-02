d78dbcef377bd7e469a577eb854929a6d2757bd7
Carlos Sanchez <caaugustoss@gmail.com>
2023-03-02 12:52:46 +0100

    add: better tests

--- src/test/scala/com/ubirch/client/protocol/SignerTest.scala
+++ src/test/scala/com/ubirch/client/protocol/SignerTest.scala
@@ -1,21 +1,31 @@
 package com.ubirch.client.protocol
 
-import java.util.UUID
 import com.ubirch.crypto.GeneratorKeyFactory
 import com.ubirch.crypto.utils.Curve
+
 import org.scalatest.flatspec.AnyFlatSpec
 
+import java.util.UUID
 import scala.util.Random
 
 class SignerTest extends AnyFlatSpec {
+
   "DefaultProtocolSigner" should "work with EDDSA keys" in {
     val key = GeneratorKeyFactory.getPrivKey(Curve.Ed25519)
     val ps = new DefaultProtocolSigner(_ => Some(key))
     val data = new Array[Byte](255)
     Random.nextBytes(data)
-    ps.sign(UUID.randomUUID(), data, 0, data.length)
-
+    val uuid = UUID.randomUUID()
+    val signature = ps.sign(uuid, data, 0, data.length)
     // the above should just not throw
+
+    val verifier = new DefaultProtocolVerifier((_: UUID) =>
+      List(GeneratorKeyFactory.getPubKey(key.getRawPublicKey, Curve.Ed25519))
+    )
+
+    val verification = verifier.verify(uuid, data, 0, data.length, signature)
+    assert(verification)
+
   }
 
   it should "work with ECDSA keys" in {
@@ -23,8 +33,17 @@ class SignerTest extends AnyFlatSpec {
     val ps = new DefaultProtocolSigner(_ => Some(key))
     val data = new Array[Byte](255)
     Random.nextBytes(data)
-    ps.sign(UUID.randomUUID(), data, 0, data.length)
+    val uuid = UUID.randomUUID()
 
+    val signature = ps.sign(uuid, data, 0, data.length)
     // the above should just not throw
+
+    val verifier = new DefaultProtocolVerifier((_: UUID) =>
+      List(GeneratorKeyFactory.getPubKey(key.getRawPublicKey, Curve.PRIME256V1))
+    )
+
+    val verification = verifier.verify(uuid, data, 0, data.length, signature)
+    assert(verification)
+
   }
 }
